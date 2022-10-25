4e6a5c8417e3427bc6cc8e3d269bd0db9591c5f0
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-10-25 12:31:42 -0600

    Added Quiz Model

--- server/models/Tech.js
+++ server/models/Tech.js
@@ -1,13 +1,26 @@
 const { Schema, model } = require('mongoose');
 
-const techSchema = new Schema({
-  name: {
+const quizSchema = new Schema({
+  question: {
     type: String,
     required: true,
     unique: true,
   },
+  choices: {
+    type: String,
+    required: true
+  },
+  firePoints: {
+    type: Number
+  },
+  grassPoints: {
+    type: Number
+  },
+  waterPoints: {
+    type: Number
+  }
 });
 
-const Tech = model('Tech', techSchema);
+const Quiz = model('Quiz', quizSchema);
 
-module.exports = Tech;
+module.exports = Quiz;
