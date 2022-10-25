73cc6cca63da17abebed24f40b513705b31883e3
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-10-25 12:41:47 -0600

    updated index with new models and exported them

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
-const Battle = require('./Battle');
-const Quiz = require('./Quiz');
-const Matchup = require('./Quiz');
-const Matchup = require('./Quiz');
+const Battle = require("./Battle");
+const Quiz = require("./Quiz");
+const User = require("./User");
+const Pokemon = require("./Pokemon");
 
-module.exports = { Tech, Matchup };
+module.exports = { Battle, Quiz, User, Pokemon };
