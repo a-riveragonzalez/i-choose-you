7d8e19b41b84c833860b65db730c704ec3864978
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-10-29 01:40:44 -0700

    added the battles page, battles page has list of battles that user is in and has a button to the battle

--- client/src/pages/UserBattles/index.js
+++ client/src/pages/UserBattles/index.js
@@ -1,34 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { Link, useNavigate } from "react-router-dom";
 import { QUERY_BATTLES } from "../../utils/queries";
+import Card from "react-bootstrap/Card";
+import AuthService from "../../utils/auth";
 
 import "./userBattles.css";
 
-// battleId : 635c8b4b473aa9387400fe2e
 const UserBattles = () => {
-
+  const navigate = useNavigate();
   const { loading, data } = useQuery(QUERY_BATTLES);
+  const [userBattleArr, setUserBattleArr] = useState([]);
 
   let battleArray = data?.battles || [];
-  console.log(battleArray)
+  console.log(battleArray);
 
+  // finds logged in user's _id
+  const userId = AuthService.getProfile().data._id;
+  console.log(userId);
+
+  // this creates three random matches from the user array
+  useEffect(() => {
+    if (battleArray.length > 0) {
+      const getUserBattles = (battle) => {
+        console.log("battle ", battle);
+        let tempBattles = [];
+
+        // todo for loop, if battles.userid === user's id then push it into temp arr then return temp arr
+        for (let i = 0; i < battleArray.length; i++) {
+          console.log("forloop : ");
+          if (
+            battle[i].user1_id._id === userId ||
+            battle[i].user2_id._id === userId
+          ) {
+            console.log("if statment");
+            tempBattles.push(battle[i]);
+          }
+        }
+
+        console.log("tempBattles", tempBattles);
+        return tempBattles;
+      };
+
+      const newBattleArray = getUserBattles(battleArray);
+      console.log("newBattleArray : ", newBattleArray);
+
+      setUserBattleArr(newBattleArray);
+    }
+  }, [battleArray]);
 
   //  handles input message and creates new message
   // const handleFormSubmit = async (event) => {
   //   event.preventDefault();
+  //   console.log(event)
 
-  //   try {
-  //     await createMessage({
-  //       variables: { battleId: id, messageContent: input },
-  //     });
-
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-
-  //   setInput("");
-  //   window.location.reload()
+  //   navigate(`/battle/${--event.something--}`);
   // };
 
   return (
@@ -39,51 +65,30 @@ const UserBattles = () => {
         <section className="container">
           {/* ************* Battle Title container************* */}
           <div className="battle-header mb-3">
-            <h4>
-              Here are your current battles
-            </h4>
+            <h4>Here are your current battles</h4>
           </div>
 
-          {/* ************* MessageList container************* */}
-          {/* <div className="text-white">
-            <ul className="message-list">
-              {messageArray.map((message) => {
-                return (
-                  <li
-                    key={message._id}
-                    className={`mb-3 text-box-message ${handleTextBoxDirection(
-                      message.user._id
-                    )}`}
-                  >
-                    <div>{message.user.username}</div>
-                    <div>{message.messageContent}</div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div> */}
-
-          {/* ************* SendMessageForm container************* */}
-          {/* <div className="text-white">
-            <div className="form-group">
-              <textarea
-                name="message"
-                value={input}
-                onChange={handleInputChange}
-                type="text"
-                placeholder="message"
-                className="form-control text-box-message"
-                rows="3"
-              />
-              <button
-                type="submit"
-                onClick={handleFormSubmit}
-                className="btn btn-light message-btn"
-              >
-                Send
-              </button>
-            </div>
-          </div> */}
+          {/* ************* Battle List container************* */}
+          <div className="text-white">
+            <section className="row">
+              {userBattleArr &&
+                userBattleArr.map((battles) => {
+                  return (
+                    <Card key={battles._id}>
+                      <Card.Body className="text-danger">
+                        {battles.user1_id.username} vs.{" "}
+                        {battles.user2_id.username}
+                      </Card.Body>
+                      <Link to= {`/battle/${battles._id}`} >
+                        <button class="btn btn-light continue-btn">
+                          Continue Battle
+                        </button>
+                      </Link>
+                    </Card>
+                  );
+                })}
+            </section>
+          </div>
         </section>
       )}
     </div>
