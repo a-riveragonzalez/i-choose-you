eb4934086e2ff2471ef54d66220a10a1123afe5f
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-10-31 02:08:47 -0700

    deleted battleData because it was causing errors and not needed anymore. added styles to userbattles

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -4,19 +4,16 @@ const { User, Pokemon, Quiz, Battle } = require('../models');
 const pokemonData = require('./pokemonData.json');
 const quizData = require('./quizData.json');
 const userData = require('./userData.json')
-const battleData = require('./battleData.json')
 
 db.once('open', async () => {
-  await Pokemon.deleteMany({});
-  await Quiz.deleteMany({});
+  // await Pokemon.deleteMany({});
+  // await Quiz.deleteMany({});
   // await User.deleteMany({});
-  // await Battle.deleteMany({})
 
 
-  const pokemongos = await Pokemon.insertMany(pokemonData);
-  const quizQuestions = await Quiz.insertMany(quizData);
+  // const pokemongos = await Pokemon.insertMany(pokemonData);
+  // const quizQuestions = await Quiz.insertMany(quizData);
   // const users = await User.insertMany(userData);
-  // const battles = await Battle.insertMany(battleData);
 
   console.log('Database seeded!');
   process.exit(0);
