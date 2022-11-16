77483267a2aff90a732d7e6be45b336a47ce643f
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-11-16 13:29:36 -0800

    edited header and background image

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
+import pokeballimg from "./smallpokeball.png";
 
-import Navigation from './Navigation';
+import Navigation from "./Navigation";
 
 const Header = () => {
-
-
   return (
-    <header className="mb-4 pt-3 text-center">
-      <div className="justify-space-between-lg justify-center align-center">
-
-          <h1 className="page-title">I ChOoSe YoU!</h1>
-
+    <header className="mb-4 pt-3 text-center ">
+      <div className="justify-space-between justify-center align-center row">
+        <div className="col-12 col-md-5">
+          <h1 className="row justify-center">
+            <img
+              src={pokeballimg}
+              alt="pokeball icon"
+              className="nav-icon "
+            ></img>{" "}
+            <p className="page-title ">I ChOoSe YoU!</p>
+          </h1>
+        </div>
+        <div className="col-12 col-md-5">
+          <Navigation />
+        </div>
       </div>
-        <Navigation/>
     </header>
   );
 };
