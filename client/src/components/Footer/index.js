a08bb459ab25add3218caec7922ba8a9657ff993
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-12-21 15:11:00 -0800

    edited the footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 import "./footer.css";
-
 
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto p-1">
       <div className="text-center mb-1 ">
-        <h4 className='footer-font'>
+        {/* <h4 className='footer-font'>
           Made with{' '}
           <span
             className="emoji"
@@ -17,7 +16,15 @@ const Footer = () => {
             ❤️
           </span>{' '}
           by Team <span className="rocket">R</span>ocket 
-        </h4>
+        </h4> */}
+        <p className="footer-font my-1">
+          Copyright <span>&copy;</span> 2022
+          <br></br>
+          Designed by Team <span className="rocket">R</span>ocket
+        </p>
+        <a href="https://github.com/a-riveragonzalez/i-choose-you">
+          <i class="fa-brands fa-github fa-lg"></i> 
+        </a>
       </div>
     </footer>
   );
