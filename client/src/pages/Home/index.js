8928489eb6ae4f26fd3a7b5cf13efe40268189d2
cindy <cindyung56@gmail.com>
2023-01-12 15:37:15 -0800

    added links for pictures

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,24 +1,38 @@
 import React from "react";
 
 import "./home.css";
+import bulbasaurImg from "./bulbasaur-in-the-woods.png"
 
 const Home = () => {
 
     return (
-      <div>
-        <h4 className="oak-warning p-2">
+      <>
+        {/* <h4 className="oak-warning p-2">
           Hey, you can't ride your bike in here! <br></br>You need to be logged
           in to see this. Use the navigation links above to sign up or log in!
-        </h4>
-        <div className="img">
-          <img
-            className="prof"
-            src="https://www.androidheadlines.com/wp-content/uploads/2020/02/Professor-Oak-Mew-Trailer-Screenshot.jpg"
-            alt="professor-oak"
-          />
+        </h4> */}
+        <div className="bulbasaur-img">
+          {/* <img
+            className=""
+            src={bulbasaurImg}
+            alt="bulbasaur"
+          /> */}
+          <div className="opening">
+            <h1>jsdfhlaksdjf</h1>
+          </div>
         </div>
-      </div>
+      </>
     );
 };
 
 export default Home;
+
+
+/*
+
+https://media.gq.com/photos/57b2026444afab8a05bf70f1/16:9/w_2560%2Cc_limit/pokemon-go-still-playing.jpg
+https://metro.co.uk/wp-content/uploads/2021/03/DSC00180-3c36.jpg?quality=90&strip=all&zoom=1&resize=480%2C319
+https://i.insider.com/5d25d7a6a17d6c2e1b3b7a15
+
+
+*/
